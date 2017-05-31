2c81fedcc315f4253247f1ea4795ad4d040b0333
PikminGuts92 <PikminGuts92@gmail.com>
2017-05-30 23:06:09 -0400

    Added AbstractEntry and CLI driver.

---
+++ Mackiloha/Milo/AbstractEntry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mackiloha.Milo
+{
+    public abstract class AbstractEntry
+    {
+        /// <summary>
+        /// Abstract milo entry
+        /// </summary>
+        /// <param name="name">Name</param>
+        /// <param name="type">Type</param>
+        /// <param name="bigEndian">Big endian?</param>
+        public AbstractEntry(string name, string type, bool bigEndian = true)
+        {
+            Name = name;
+            Type = type;
+            BigEndian = bigEndian;
+        }
+
+        public string Name { get; set; }
+        public virtual string Type { get; }
+        public abstract byte[] Data { get; }
+        public bool BigEndian { get; set; }
+    }
+}
---
+++ Porkchop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mackiloha.Milo;
+
+namespace Porkchop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
