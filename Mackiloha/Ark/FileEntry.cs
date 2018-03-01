d0f5900a6c6e724b218177ddede58b64f40ba054
PikminGuts92 <PikminGuts92@gmail.com>
2018-03-01 00:06:44 -0500

    Started re-writing file import

---
+++ Mackiloha/Ark/FileEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Mackiloha.Ark
+{
+    public abstract class FileEntry
+    {
+        private readonly static Regex _directoryRegex = new Regex(@"^[_\-a-zA-Z0-9]|([/][_\-a-zA-Z0-9]+)*$");
+        private readonly static Regex _fileRegex = new Regex(@"^[_\-a-zA-Z0-9]+[.]?[_\-a-zA-Z0-9]*$");
+
+        public string FileName { get; }
+        public string DirectoryName { get; }
+
+        private bool IsValidPath(string text, bool directory = false)
+        {
+            if (directory)
+                return _directoryRegex.IsMatch(text) || (text == string.Empty);
+
+            return _fileRegex.IsMatch(text);
+        }
+    }
+}
