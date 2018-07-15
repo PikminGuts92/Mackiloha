c60653caf6e8fb43791a8ff389fbca633809ae52
PikminGuts92 <PikminGuts92@gmail.com>
2018-07-15 19:02:44 -0400

    Added toolbar and milo user control

--- MiloStardust/MainWindow.xaml.cs
+++ MiloStardust/MainWindow.xaml.cs
@@ -24,5 +24,49 @@ namespace MiloStardust
         {
             InitializeComponent();
         }
+
+        private void ToolBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Hides the stupid overflow arrow
+            // Source: http://stackoverflow.com/questions/4662428/how-to-hide-arrow-on-right-side-of-a-toolbar
+
+            ToolBar toolBar = sender as ToolBar;
+            var overflowGrid = toolBar.Template.FindName("OverflowGrid", toolBar) as FrameworkElement;
+            if (overflowGrid != null)
+            {
+                overflowGrid.Visibility = Visibility.Collapsed;
+            }
+
+            var mainPanelBorder = toolBar.Template.FindName("MainPanelBorder", toolBar) as FrameworkElement;
+            if (mainPanelBorder != null)
+            {
+                mainPanelBorder.Margin = new Thickness(0);
+            }
+        }
+
+        private void Menu_File_Open_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_File_SaveAs_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_File_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Menu_Tools_Options_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Help_About_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
