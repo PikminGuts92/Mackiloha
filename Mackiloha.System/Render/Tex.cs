c2b15d9b64368e71f3dd2406ca98104a003f6bb3
PikminGuts92 <PikminGuts92@gmail.com>
2018-11-07 00:33:49 -0500

    Added texture object

---
+++ Mackiloha.System/Render/Tex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mackiloha.System.Render
+{
+    public class Tex : RenderObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Bpp { get; set; }
+
+        public MiloString ExternalPath { get; set; }
+        public bool UseExternal { get; set; }
+
+        public HMXBitmap Bitmap { get; set; }
+    }
+}
