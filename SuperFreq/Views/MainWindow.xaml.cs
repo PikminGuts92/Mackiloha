147f0f1ef5131ed5f457d06a1751cba721c009f5
PikminGuts92 <PikminGuts92@gmail.com>
2019-03-06 21:58:21 -0500

    Added Avalonia project

---
+++ SuperFreq/Views/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace SuperFreq.Views
+{
+    public class MainWindow : Window
+    {
+        private readonly OpenFileDialog OFD = new OpenFileDialog();
+        private readonly string OrigTitle;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            OrigTitle = this.Title;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private async void Menu_File_Open_Click(object sender, RoutedEventArgs e)
+        {
+            OFD.AllowMultiple = false;
+            var results = await OFD.ShowAsync(this);
+
+            if (results.Length <= 0)
+                return;
+
+            this.Title = $"{OrigTitle} - {System.IO.Path.GetFileName(results.First())}";
+        }
+
+        private void Menu_File_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
