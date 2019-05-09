7a68fa06703635f46f942a77754b252e3cc3e06c
PikminGuts92 <PikminGuts92@gmail.com>
2019-05-08 23:43:21 -0400

    Added tree view

--- SuperFreq/MainWindow.xaml.cs
+++ SuperFreq/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Mackiloha.IO;
+using Mackiloha.Milo2;
+using Mackiloha.App;
+using Mackiloha.App.Extensions;
 
 namespace SuperFreq
 {
+    public class Node
+    {
+        public Node()
+        {
+
+        }
+
+        public Node(string name)
+        {
+            Name = name;
+        }
+
+        public List<Node> Children { get; set; } = new List<Node>();
+        public string Name { get; set; }
+    }
+
+    public class IconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // FolderSolid, FolderOpenSolid, FileAltSolid
+            var node = value as Node;
+
+            return (node == null || node.Children.Count > 0) ? "FolderSolid" : "FileAltSolid";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,6 +60,29 @@ namespace SuperFreq
         public MainWindow()
         {
             InitializeComponent();
+
+            var root = new Node()
+            {
+                Name = "Root",
+                Children = new List<Node>()
+                {
+                    new Node("Entry 1"),
+                    new Node("Entry 2")
+                    {
+                        Children = new List<Node>()
+                        {
+                            new Node("Sub Entry a"),
+                            new Node("Sub Entry b"),
+                            new Node("Sub Entry c")
+                        }
+                    },
+                    new Node("Entry 3"),
+                }
+            };
+
+            var rooObj = new Node() { Children = new List<Node>() { root } };
+
+            TreeView_MiloEntries.DataContext = rooObj;
         }
 
         private void ToolBar_Loaded(object sender, RoutedEventArgs e)
