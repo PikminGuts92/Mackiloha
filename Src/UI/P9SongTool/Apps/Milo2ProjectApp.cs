1a4f49b03a21fbce575d5951c77471d72addaf00
PikminGuts92 <PikminGuts92@gmail.com>
2020-09-24 19:38:29 -0400

    Implement lipsync extract

--- Src/UI/P9SongTool/Apps/Milo2ProjectApp.cs
+++ Src/UI/P9SongTool/Apps/Milo2ProjectApp.cs
@@ -3,6 +3,7 @@ using Mackiloha.App;
 using Mackiloha.App.Extensions;
 using Mackiloha.IO;
 using Mackiloha.Milo2;
+using P9SongTool.Exceptions;
 using P9SongTool.Options;
 using System;
 using System.Collections.Generic;
@@ -15,12 +16,21 @@ namespace P9SongTool.Apps
 {
     public class Milo2ProjectApp
     {
+        protected readonly string[] Characters = new[] { "john", "george", "paul", "ringo"  };
+
         public void Parse(Milo2ProjectOptions op)
         {
             var appState = AppState.FromFile(op.InputPath);
             appState.UpdateSystemInfo(GetSystemInfo(op));
 
             var milo = appState.OpenMiloFile(op.InputPath);
+
+            // Create output directory
+            if (!Directory.Exists(op.OutputPath))
+                Directory.CreateDirectory(op.OutputPath);
+
+            var entries = GetEntries(milo);
+            ExtractLipsync(entries, op.OutputPath);
         }
 
         protected SystemInfo GetSystemInfo(Milo2ProjectOptions op)
@@ -34,5 +44,45 @@ namespace P9SongTool.Apps
                     ? Platform.PS3
                     : Platform.X360
             };
+
+        protected List<MiloObject> GetEntries(MiloObjectDir miloDir)
+        {
+            var entries = new List<MiloObject>();
+            GetEntries(miloDir, entries);
+            return entries;
+        }
+
+        protected void GetEntries(MiloObjectDir miloDir, List<MiloObject> entries)
+        {
+            if (miloDir.Type != "ObjectDir")
+                throw new UnsupportedMiloException($"Directory type of \"{miloDir.Type}\" found, expected \"ObjectDir\" expected");
+
+            // Traverse sub directories
+            if (!(miloDir.Extras["DirectoryEntry"] is MiloObjectDirEntry dirEntry))
+                throw new UnsupportedMiloException("Could not parse directory entry");
+
+            foreach (var subDir in dirEntry.SubDirectories)
+            {
+                GetEntries(subDir, entries);
+            }
+
+            // Add entries in current directory
+            entries.AddRange(miloDir.Entries);
+        }
+
+        protected void ExtractLipsync(List<MiloObject> entries, string dirPath)
+        {
+            var lipsyncEntries = entries
+                .Where(x => x.Type == "CharLipSync")
+                .ToList();
+
+            foreach (var lipsync in lipsyncEntries)
+            {
+                var lipFilePath = Path.Combine(dirPath, lipsync.Name);
+
+                var lipBytes = lipsync as MiloObjectBytes; // Should always be this
+                File.WriteAllBytes(lipFilePath, lipBytes.Data);
+            }
+        }
     }
 }
---
+++ Src/UI/P9SongTool/Exceptions/UnsupportedMiloException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P9SongTool.Exceptions
+{
+    public class UnsupportedMiloException : Exception
+    {
+        public UnsupportedMiloException(): this("Milo is unsupported")
+        {
+
+        }
+
+        public UnsupportedMiloException(string message) : base(message)
+        {
+
+        }
+    }
+}
