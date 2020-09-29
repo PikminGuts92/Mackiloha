f25e26b8e76af2a50bca2a93246c8cd711612ce6
PikminGuts92 <PikminGuts92@gmail.com>
2020-09-28 23:35:47 -0400

    Started implementing proj to milo

--- Src/UI/P9SongTool/Apps/Project2MiloApp.cs
+++ Src/UI/P9SongTool/Apps/Project2MiloApp.cs
@@ -3,21 +3,55 @@ using Mackiloha.App;
 using Mackiloha.App.Extensions;
 using Mackiloha.IO;
 using Mackiloha.Milo2;
+using P9SongTool.Exceptions;
+using P9SongTool.Models;
 using P9SongTool.Options;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 
 namespace P9SongTool.Apps
 {
     public class Project2MiloApp
     {
+        protected readonly (string extension, string miloType)[] SupportedExtraTypes;
+
+        public Project2MiloApp()
+        {
+            SupportedExtraTypes = new (string extension, string miloType)[]
+            {
+                (".anim", "PropAnim"),
+                (".mat", "Mat"),
+                (".tex", "Tex")
+            };
+        }
+
         public void Parse(Project2MiloOptions op)
         {
-            throw new NotImplementedException();
+            if (!Directory.Exists(op.InputPath))
+                throw new MiloBuildException("Input path doesn't exist");
+
+            var inputDir = Path.GetFullPath(op.InputPath);
+            var songMetaPath = Path.Combine(inputDir, "song.json");
+            var midPath = Path.Combine(inputDir, "venue.mid");
+
+            var lipsyncPaths = Directory.GetFiles(Path.Combine(inputDir, "lipsync"), "*.lipsync");
+            var extrasPaths = Directory.GetFiles(Path.Combine(inputDir, "extra"));
+
+            var p9song = OpenP9File(songMetaPath);
+
+        }
+
+        protected P9Song OpenP9File(string p9songPath)
+        {
+            var appState = AppState.FromFile(p9songPath);
+            var jsonText = File.ReadAllText(p9songPath);
+
+            return JsonSerializer.Deserialize<P9Song>(jsonText, appState.JsonSerializerOptions);
         }
     }
 }
---
+++ Src/UI/P9SongTool/Exceptions/MiloBuildException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P9SongTool.Exceptions
+{
+    public class MiloBuildException : Exception
+    {
+        public MiloBuildException(string message) : base(message)
+        {
+
+        }
+    }
+}
