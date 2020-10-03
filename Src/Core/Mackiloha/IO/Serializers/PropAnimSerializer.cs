9d7a782022f4dac53cd20fa2dd79b5d92a296b9a
PikminGuts92 <PikminGuts92@gmail.com>
2020-10-03 12:23:35 -0400

    Implemented writing for prop anim serializer

--- Src/Core/Mackiloha/IO/Serializers/PropAnimSerializer.cs
+++ Src/Core/Mackiloha/IO/Serializers/PropAnimSerializer.cs
@@ -1,6 +1,7 @@
 ﻿using Mackiloha.Song;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Mackiloha.IO.Serializers
@@ -125,7 +126,145 @@ namespace Mackiloha.IO.Serializers
 
         public override void WriteToStream(AwesomeWriter aw, ISerializable data)
         {
-            throw new NotImplementedException();
+            var propAnim = data as PropAnim;
+
+            var version = Magic();
+
+            // Hacky way to support GDRB
+            if (version == 11 && IsGRDRBAnim(propAnim))
+                version = 12;
+
+            aw.Write(version);
+
+            // Write meta
+            aw.Write((int)2);
+            aw.Write((string)propAnim.AnimName);
+            aw.BaseStream.Position += 5; // Skip zeros
+
+            aw.Write((int)4);
+            aw.Write((float)propAnim.TotalTime);
+            aw.Write((int)1);
+
+            if (version >= 12)
+                aw.Write((byte)0x00);
+
+            // Write director groups
+            aw.Write(propAnim.DirectorGroups.Count);
+            foreach (var group in propAnim.DirectorGroups)
+                WriteGroupEvent(group, aw);
+        }
+
+        protected void WriteGroupEvent(DirectedEventGroup eventGroup, AwesomeWriter aw)
+        {
+            // Write event type + director
+            aw.Write((int)eventGroup.EventType);
+            aw.Write((int)eventGroup.EventType);
+            aw.Write((string)eventGroup.DirectorName);
+
+            // Constants
+            aw.Write((bool)true);
+            aw.Write((bool)false);
+            aw.Write((bool)true);
+            aw.Write((int)0);
+            aw.Write((int)5);
+
+            // Write prop names
+            aw.Write((string)eventGroup.PropName);
+            aw.Write((int)0);
+            aw.Write((string)eventGroup.PropName2);
+
+            // Unknown (probably some kind of enum or flag, usually 0)
+            aw.Write((int)0);
+
+            if (eventGroup.Events is null)
+            {
+                aw.Write((int)0);
+                return;
+            }
+
+            // Write events
+            aw.Write((int)eventGroup.Events.Count);
+
+            switch (eventGroup.EventType)
+            {
+                case DirectedEventType.Float:
+                    var fEvents = CastEvents<DirectedEventFloat>(eventGroup.Events);
+
+                    foreach (var ev in fEvents)
+                    {
+                        aw.Write((float)ev.Value);
+                        aw.Write((float)ev.Position);
+                    }
+                    break;
+                case DirectedEventType.TextFloat:
+                    var tfEvents = CastEvents<DirectedEventTextFloat>(eventGroup.Events);
+
+                    foreach (var ev in tfEvents)
+                    {
+                        aw.Write((float)ev.Value);
+                        aw.Write((string)ev.Text);
+                        aw.Write((float)ev.Position);
+                    }
+                    break;
+                case DirectedEventType.Boolean:
+                    var bEvents = CastEvents<DirectedEventBoolean>(eventGroup.Events);
+
+                    foreach (var ev in bEvents)
+                    {
+                        aw.Write((bool)ev.Enabled);
+                        aw.Write((float)ev.Position);
+                    }
+                    break;
+                case DirectedEventType.Vector4:
+                    var v4Events = CastEvents<DirectedEventVector4>(eventGroup.Events);
+
+                    foreach (var ev in v4Events)
+                    {
+                        aw.Write((float)ev.Value.X);
+                        aw.Write((float)ev.Value.Y);
+                        aw.Write((float)ev.Value.Z);
+                        aw.Write((float)ev.Value.W);
+                        aw.Write((float)ev.Position);
+                    }
+                    break;
+                case DirectedEventType.Vector3:
+                    var v3Events = CastEvents<DirectedEventVector3>(eventGroup.Events);
+
+                    foreach (var ev in v3Events)
+                    {
+                        aw.Write((float)ev.Value.X);
+                        aw.Write((float)ev.Value.Y);
+                        aw.Write((float)ev.Value.Z);
+                        aw.Write((float)ev.Position);
+                    }
+                    break;
+                case DirectedEventType.Text:
+                    var tEvents = CastEvents<DirectedEventText>(eventGroup.Events);
+
+                    foreach (var ev in tEvents)
+                    {
+                        aw.Write((string)ev.Text);
+                        aw.Write((float)ev.Position);
+                    }
+                    break;
+            }
+        }
+
+        protected List<T> CastEvents<T>(List<IDirectedEvent> events)
+            where T : IDirectedEvent
+            => events
+                .Select(x => (T)Convert.ChangeType(x, typeof(T)))
+                .ToList();
+
+        protected bool IsGRDRBAnim(PropAnim anim)
+        {
+            var gdrbNames = new[] { "_mikedirnt", "_trecool", "_billiejoe" };
+
+            // Check if any events are for GDRB specific band members
+            return anim.DirectorGroups
+                .Select(x => x.PropName)
+                .Any(x => gdrbNames
+                    .Any(y => x.EndsWith(y, StringComparison.CurrentCultureIgnoreCase)));
         }
 
         public override bool IsOfType(ISerializable data) => data is PropAnim;
