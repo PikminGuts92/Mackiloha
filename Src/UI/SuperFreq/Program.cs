ee8dfcab3ef098066990a9e7ca57e1e281c65ea9
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2023-11-17 22:13:26 -0500

    Remove crypt options in super freq

--- Src/UI/SuperFreq/Program.cs
+++ Src/UI/SuperFreq/Program.cs
@@ -7,7 +7,6 @@ namespace SuperFreq;
 class Program
 {
     // Fixes AOT for CommandLine
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(CryptOptions))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(Dir2MiloOptions))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(Milo2DirOptions))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(PngToTextureOptions))]
@@ -21,12 +20,10 @@ class Program
 
         // TODO: Make pretty
         Parser.Default.ParseArguments<
-            CryptOptions,
             Dir2MiloOptions,
             Milo2DirOptions,
             PngToTextureOptions,
             TextureToPngOptions>(args)
-            .WithParsed<CryptOptions>(CryptOptions.Parse)
             .WithParsed<Dir2MiloOptions>(Dir2MiloOptions.Parse)
             .WithParsed<Milo2DirOptions>(Milo2DirOptions.Parse)
             .WithParsed<PngToTextureOptions>(PngToTextureOptions.Parse)
